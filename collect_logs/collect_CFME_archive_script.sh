9061c0361d3a54180fb912197768de7c7f533208
Jared Deubel <jdeubel@redhat.com>
2016-10-25 13:27:43 -0400

    Pushing to new repo for broken out versions

--- collect_logs/collect_CFME_archive_script.sh
+++ collect_logs/collect_CFME_archive_script.sh
@@ -2,7 +2,7 @@
 collect_logs_directory=$(pwd)
 # save directory from which command is initiated
 pushd /var/www/miq/vmdb
-# make the vmdb/log directory the current directory
+# make the vmdb/log directory the current directory 
 rm -f log/evm_full_archive_$(uname -n)* log/evm_current_$(uname -n)*
 # eliminiate any prior collected logs to make sure that only one collection is current
 
@@ -10,24 +10,11 @@ rm -f log/evm_full_archive_$(uname -n)* log/evm_current_$(uname -n)*
 read tbuild < BUILD
 echo "$tbuild"
 subset=${tbuild:0:3}
-echo "$subset"
-if [ $subset = "5.5" ]; then
-     echo 'this is a CFME 40 environment'
-     else
-     echo 'this is not a CFME 40 environment'
-fi
+echo CloudForms "$subset"
 
 case $subset in
-"5.5" )
- message="cloudforms 4.0 release"
- if [ -e "/var/opt/rh/rh-postgresql94/lib/pgsql/data/postgresql.conf" ] ; then
-  postgresql_path_files="/var/opt/rh/rh-postgresql94/lib/pgsql/data/*.conf /var/opt/rh/rh-postgresql94/lib/pgsql/data/pg_log/* "
-  else
-  echo "this appliance does not contain a running postgresql instance, no postgresql materials collected"
- fi
- ;;
-"5.6" )
- message="cloudforms 4.1 release"
+"5.5"|"5.6"|"5.7")
+ message="cloudforms 4.* release"
  if [ -e "/var/opt/rh/rh-postgresql94/lib/pgsql/data/postgresql.conf" ] ; then
   postgresql_path_files="/var/opt/rh/rh-postgresql94/lib/pgsql/data/*.conf /var/opt/rh/rh-postgresql94/lib/pgsql/data/pg_log/* "
   else
@@ -35,11 +22,17 @@ case $subset in
  fi
  ;;
 *)
- message="unknown cloudforms release"
+ message="unknown cloudforms release, log collection terminated "
  ;;
 esac
-# then collect all appropriate logs into one tgz
-echo "XZ_OPT=-9 tar -cJvf log/evm_full_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION log/* config/*  /var/log/* $postgresql_path_files  "
-XZ_OPT=-9 tar -cJvf log/evm_full_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION log/* config/* /var/log/* $postgresql_path_files
-# then restore prior current directory
+
+
+if [ -e /opt/rh/postgresql92/root/var/lib/pgsql/data/pg_log/postgresql.log ]
+then
+echo "XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $postgresql_path_files "
+else
+XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/* $postgresql_path_files
+fi
+
+# and restore previous current directory
 popd
