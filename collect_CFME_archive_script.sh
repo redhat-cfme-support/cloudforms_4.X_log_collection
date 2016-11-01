b8e012048c7a556fe357ff7180b3a37cc875d8a0
Jared Deubel <jdeubel@redhat.com>
2016-11-01 12:41:58 -0400

    Update collect_CFME_archive_script.sh

--- collect_CFME_archive_script.sh
+++ collect_CFME_archive_script.sh
@@ -3,7 +3,7 @@ collect_logs_directory=$(pwd)
 # save directory from which command is initiated
 pushd /var/www/miq/vmdb
 # make the vmdb/log directory the current directory 
-rm -f log/evm_full_archive_$(uname -n)* log/evm_current_$(uname -n)*
+rm -f log/evm_full_archive_$(uname -n)* log/evm_archived_$(uname -n)*
 # eliminiate any prior collected logs to make sure that only one collection is current
 
 # determine what level of CFME this command is executing on
@@ -18,20 +18,20 @@ case $subset in
  if [ -e "/var/opt/rh/rh-postgresql94/lib/pgsql/data/postgresql.conf" ] ; then
   postgresql_path_files="/var/opt/rh/rh-postgresql94/lib/pgsql/data/*.conf /var/opt/rh/rh-postgresql94/lib/pgsql/data/pg_log/* "
   else
-  echo "this appliance does not contain a running postgresql instance, no postgresql materials collected"
+  echo "This appliance does not contain a running Postgresql instance, no Postgresql materials collected"
  fi
  ;;
 *)
- message="unknown cloudforms release, log collection terminated "
+ message="Unknown Cloudforms release, log collection terminated "
  ;;
 esac
 
 
 #if [ -e /opt/rh/postgresql92/root/var/lib/pgsql/data/pg_log/postgresql.log ]
 #then
-echo "XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $postgresql_path_files "
+echo "XZ_OPT=-9 tar -cJvf log/evm_archived_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $postgresql_path_files "
 #else
-XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/* $postgresql_path_files
+XZ_OPT=-9 tar -cJvf log/evm_archived_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/* $postgresql_path_files
 #fi
 
 # and restore previous current directory
