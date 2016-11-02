e9094fd0cb9fc36affb11f7e389b8c95ad548baa
Jared Deubel <jdeubel@redhat.com>
2016-11-02 14:17:49 -0400

    Updated code for pulling in env variable

--- collect_CFME_archive_script.sh
+++ collect_CFME_archive_script.sh
@@ -14,9 +14,9 @@ source /etc/default/evm
 #$(APPLIANCE_PG_DATA)
 
 
-if [ -e $APPLIANCE_PG_DATA/pg_log ]
+if [ -d "$APPLIANCE_PG_DATA" ]
 then
-echo "XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf"
+XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
 else
 XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/*
 fi
