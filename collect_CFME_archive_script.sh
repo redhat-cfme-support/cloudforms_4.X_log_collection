bbf50254e6766a02ade0aa53bb8bf7fe480a3b48
Jared Deubel <jdeubel@redhat.com>
2016-11-02 14:21:39 -0400

    Updated naming convention

--- collect_CFME_archive_script.sh
+++ collect_CFME_archive_script.sh
@@ -9,18 +9,14 @@ rm -f log/evm_full_archive_$(uname -n)* log/evm_current_$(uname -n)*
 #Source in the file so that we can call postgresql functions
 source /etc/default/evm
 
-
-#$(APPLIANCE_PG_DATA) 
-#$(APPLIANCE_PG_DATA)
-
-
 if [ -d "$APPLIANCE_PG_DATA" ]
 then
-XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
+XZ_OPT=-9 tar -cJvf log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
+echo "Has Postgresql Directories"
 else
-XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/*
+XZ_OPT=-9 tar -cJvf log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/*
+echo "Doesn't have Postgresql Directories"
 fi
-
 # and restore previous current directory
 popd
 ~     
