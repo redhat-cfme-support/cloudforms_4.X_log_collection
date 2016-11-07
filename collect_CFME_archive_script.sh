53700077464f076019aa91d0e43a23e5e3d18c87
Jared Deubel <jdeubel@redhat.com>
2016-11-07 15:56:45 -0500

    Corrected

--- collect_CFME_archive_script.sh
+++ collect_CFME_archive_script.sh
@@ -9,17 +9,16 @@ rm -f log/evm_full_archive_$(uname -n)* log/evm_archived_$(uname -n)*
 #Source in the file so that we can call postgresql functions
 source /etc/default/evm
 
-if [ -f "$APPLIANCE_PG_DATA" ]
+if [ -d "$APPLIANCE_PG_DATA" ]
 then
 echo "This CloudForms appliance has a Database server and is running version: $(psql --version)"
 echo " Log collection starting:"
-XZ_OPT=-9 tar -cJvf log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
+XZ_OPT=-9 tar -cJvf log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.gz config/* /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
 
 else
 echo "This CloudForms appliance is not a Database server"
 echo " Log collection starting:"
-XZ_OPT=-9 tar -cJvf log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.txt config/*  /var/log/* log/apache/*
+XZ_OPT=-9 tar -cJvf log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/*.log log/*.gz config/* /var/log/* log/apache/*
 fi
 # and restore previous current directory
 popd
-~     
