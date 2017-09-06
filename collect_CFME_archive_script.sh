debfb5714c6469ccc6e02b6c09c88a6f48b697bc
Brian Duffy <bmduffy@users.noreply.github.com>
2017-09-06 19:50:16 +0100

    inform the user where the archive was written

--- collect_CFME_archive_script.sh
+++ collect_CFME_archive_script.sh
@@ -3,8 +3,9 @@
 # save directory from which command is initiated
 collect_logs_directory=$(pwd)
 
-# make the vmdb/log directory the current directory 
-pushd /var/www/miq/vmdb
+# make the vmdb/log directory the current directory
+vmdb_logs_directory="/var/www/miq/vmdb"
+pushd ${vmdb_logs_directory}
 
 # eliminiate any prior collected logs to make sure that only one collection is current
 rm -f log/evm_full_archive_$(uname -n)* log/evm_archived_$(uname -n)*
@@ -12,27 +13,20 @@ rm -f log/evm_full_archive_$(uname -n)* log/evm_archived_$(uname -n)*
 #Source in the file so that we can call postgresql functions
 source /etc/default/evm
 
+tarball="log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz"
 
-output_tarball="log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz"
-
-if [ -z "$APPLIANCE_PG_DATA"] && [ -d "$APPLIANCE_PG_DATA" ]; then
-
+if [[ -z "$APPLIANCE_PG_DATA" && -d "$APPLIANCE_PG_DATA" ]]; then
     echo "This CloudForms appliance has a Database server and is running version: $(psql --version)"
     echo " Log collection starting:"
-
-    XZ_OPT=-9 tar -cJvf ${output_tarball} --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/* config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
-
+    XZ_OPT=-9 tar -cJvf ${tarball} --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/* config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
 else
-
     echo "This CloudForms appliance is not a Database server"
     echo " Log collection starting:"
-
-    XZ_OPT=-9 tar -cJvf log/evm_archive_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/* config/* /var/log/* log/apache/*
-
+    XZ_OPT=-9 tar -cJvf ${tarball} --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION log/* config/* /var/log/* log/apache/*
 fi
-
-echo "Copy ${ouput_tarball} to ${collect_logs_directory}"
-cp ${output_tarball} ${collect_logs_directory}
 
 # and restore previous current directory
 popd
+
+# let the user know where the archive is
+echo "Archive Written To: ${vmdb_logs_directory}/${tarball}"
