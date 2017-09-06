07631174f1c43d897af0c1175dd8fba685c653a7
Brian Duffy <brian.duffy@ammeon.com>
2017-09-06 12:27:47 +0100

    added check for empty string for database env variable, and copy archive to collection directory so the user can find it easily

--- collect_CFME_current_script.sh
+++ collect_CFME_current_script.sh
@@ -1,27 +1,38 @@
 #!/bin/bash
-collect_logs_directory=$(pwd)
+
 # save directory from which command is initiated
-pushd /var/www/miq/vmdb
+collect_logs_directory=$(pwd)
+
 # make the vmdb/log directory the current directory 
-rm -f log/evm_full_archive_$(uname -n)* log/evm_current_$(uname -n)*
+pushd /var/www/miq/vmdb
+
 # eliminiate any prior collected logs to make sure that only one collection is current
+rm -f log/evm_full_archive_$(uname -n)* log/evm_current_$(uname -n)*
 
 #Source in the file so that we can call postgresql functions
 source /etc/default/evm
 
-if [ -d "$APPLIANCE_PG_DATA" ]
-then
+output_tarball="log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz"
 
-echo "This CloudForms appliance has a Database server and is running version: $(psql --version)"
-echo " Log collection starting:"
-XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/*
+if [ -z "$APPLIANCE_PG_DATA"] && [ -d "$APPLIANCE_PG_DATA" ]; then
+
+    echo "This CloudForms appliance has a Database server and is running version: $(psql --version)"
+    echo " Log collection starting:"
+
+    XZ_OPT=-9 tar -cJvf ${output_tarball} --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/*
 $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
 
 else
-echo "This CloudForms appliance is not a Database server"
-echo " Log collection starting:"
-XZ_OPT=-9 tar -cJvf log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/*
+
+    echo "This CloudForms appliance is not a Database server"
+    echo " Log collection starting:"
+
+    XZ_OPT=-9 tar -cJvf ${output_tarball} --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/*
 
 fi
+
+echo "Copy ${output_tarball} to ${collect_logs_directory}"
+cp ${output_tarball} ${collect_logs_directory}
+
 # and restore previous current directory
 popd
