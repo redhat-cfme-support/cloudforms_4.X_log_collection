cbef0036c0949246b60bb642912ef8223eac694f
Nick LaMuro <nicklamuro@gmail.com>
2018-12-19 15:28:15 -0600

    DRY up collection scripts
    Both the `archive` and `current` scripts share 95% of the same code, so
    this takes the majority of that code, and puts it in a combined script,
    and adds some variables to assist in the parts that change between the
    two.  Those can be set within the scripts themselves, or even added to
    the main script instead.
    
    A wrapper script is current required to handle the `exclude_files` file,
    since it needs to be read from the directory that the script is coming
    from.  This will most likely be changed in the future to make require
    less boiler plate code in the helper scripts.

--- collect_CFME_current_script.sh
+++ collect_CFME_current_script.sh
@@ -1,35 +1,14 @@
 #!/bin/bash
 
-# save directory from which command is initiated
-collect_logs_directory=$(pwd)
+collection_type="current"
+vmdb_logs_files="log/*.log log/*.txt"
 
-# make the vmdb/log directory the current directory 
-vmdb_logs_directory="/var/www/miq/vmdb"
-pushd ${vmdb_logs_directory}
-
-# eliminiate any prior collected logs to make sure that only one collection is current
-rm -f log/evm_full_archive_$(uname -n)* \
-      log/evm_archived_$(uname -n)*     \
-      log/evm_archive_$(uname -n)*      \
-      log/evm_current_$(uname -n)*
-
-#Source in the file so that we can call postgresql functions
-source /etc/default/evm
-
-tarball="log/evm_current_$(uname -n)_$(date +%Y%m%d_%H%M%S).tar.xz"
-
-if [[ -n "$APPLIANCE_PG_DATA" && -d "$APPLIANCE_PG_DATA/pg_log" ]]; then
-    echo "This CloudForms appliance has a Database server and is running version: $(psql --version)"
-    echo " Log collection starting:"
-    XZ_OPT=-9 tar -cJvf ${tarball} --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
+if [[ -e ${BASH_SOURCE%/*} ]]; then
+  starting_dir=$(pwd)
+  cd ${BASH_SOURCE%/*}
+  source ./collect_CFME_logs.sh
+  cd $starting_dir
 else
-    echo "This CloudForms appliance is not a Database server"
-    echo " Log collection starting:"
-    XZ_OPT=-9 tar -cJvf ${tarball} --sparse -X $collect_logs_directory/exclude_files   BUILD GUID VERSION REGION log/*.log log/*.txt config/* /var/log/* log/apache/*
+  echo "ERROR:  Unable to find collect log script!"
+  exit 1
 fi
-
-# and restore previous current directory
-popd
-
-# let the user know where the archive is
-echo "Archive Written To: ${vmdb_logs_directory}/${tarball}"
---
+++ collect_CFME_logs.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# save directory from which command is initiated
+collect_logs_directory=$(pwd)
+
+# used in filenames and globs
+vmdb_hostname=$(uname -n)
+
+# base name for tar
+tar_base_name="log/evm_full_${collection_type}_${vmdb_hostname}"
+
+# make the vmdb/log directory the current directory
+vmdb_logs_directory="/var/www/miq/vmdb"
+pushd ${vmdb_logs_directory}
+
+# eliminiate any prior collected logs to make sure that only one collection is current
+rm -f log/evm_full_archive_$vmdb_hostname* \
+      log/evm_archived_$vmdb_hostname*     \
+      log/evm_archive_$vmdb_hostname*      \
+      log/evm_current_$vmdb_hostname*
+
+# Source in the file so that we can call postgresql functions
+source /etc/default/evm
+
+tarball="${tar_base_name}_$(date +%Y%m%d_%H%M%S).tar.xz"
+
+if [[ -n "$APPLIANCE_PG_DATA" && -d "$APPLIANCE_PG_DATA/pg_log" ]]; then
+    echo "This CloudForms appliance has a Database server and is running version: $(psql --version)"
+    echo " Log collection starting:"
+    XZ_OPT=-9 tar -cJvf ${tarball} --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION $vmd_log_files config/*  /var/log/* log/apache/* $APPLIANCE_PG_DATA/pg_log/* $APPLIANCE_PG_DATA/postgresql.conf
+else
+    echo "This CloudForms appliance is not a Database server"
+    echo " Log collection starting:"
+    XZ_OPT=-9 tar -cJvf ${tarball} --sparse -X $collect_logs_directory/exclude_files BUILD GUID VERSION REGION $vmd_log_files config/* /var/log/* log/apache/*
+fi
+
+# and restore previous current directory
+popd
+
+# let the user know where the archive is
+echo "Archive Written To: ${vmdb_logs_directory}/${tarball}"
